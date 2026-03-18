e5fd3f3ee754ccb7b4ec826eab942e8c49adf8f5
genggreta2026 <genggreta1@gmail.com>
2026-03-18 21:48:27 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -5,7 +5,7 @@ import requests
 app = Flask(__name__)
 
 # ====================== 在这里填你的谷歌表格ID ======================
-GOOGLE_SHEET_ID = "https://docs.google.com/spreadsheets/d/1RwLOYYzNj3L46Q-6aC01WvcKaqfnNvL9CmHJZfjYKLI/edit?gid=0#gid=0"
+GOOGLE_SHEET_ID = "1RwLOYYzNj3L46Q-6aC01WvcKaqfnNvL9CmHJZfjYKLI/edit"
 # ===================================================================
 
 @app.route('/')
